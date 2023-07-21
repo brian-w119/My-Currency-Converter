af0d43edffebb0bfc10aa3f28cb74a281b058f81
MyPc <brianw119@outlook.com>
2023-07-21 05:39:11 +0100

    Get exchange rates and currencies

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+//API Providers
+
+const ipData = {  // get users local currency
+    key: "13913952c49cd907f6b9a168b62b0b2364ba3eb022e303cd6aec6afb8cd",
+    baseURL: "https://api.ipdata.co",
+    currency: function(){
+        return `${this.baseURL}/currency?api-key=${this.key}`;
+    },
+};
+
+
+
+const currencyLayer = {
+    baseUrl: 'http://api.currencylayer.com',
+    key: '816aace5f867a203d108dde0987d9681',
+
+    convert: function(from, to, amount) {//get exchange rate
+        return `${this.baseUrl}/convert?from=${from}&to=${to}&amount=${amount}&access_key=${this.key}`;
+      },
+
+      list: function(){//list currencies
+        return `${this.baseUrl}/list?access_key=${this.key}`;
+      },
+      
+
+    };
